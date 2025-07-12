6e94e60980294188be8a20f13c2b84fe5c8a647f
hamedkhodami <hamedkhodami977@gmail.com>
2025-07-12 16:25:00 +0330

    run project

---
+++ src/config/settings/development.py
@@ -0,0 +1,13 @@
+from .base import BASE_DIR
+import os
+# ----------------------------------------------------------------
+
+
+# --DATABASES-----------------------------------------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.getenv('SQLITE_PATH', BASE_DIR / 'db.sqlite3'),
+    }
+}
+# ----------------------------------------------------------------
